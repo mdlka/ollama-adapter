6fd4f723e0273c9fbfd309b1dd26d6c603e34e2c
mdlka <mogilevskiy499@gmail.com>
2025-04-07 21:20:35 +0300

    Create resume_extractor.rb

---
+++ lib/ollama_adapter/resume_extractor.rb
@@ -0,0 +1,16 @@
+module OllamaAdapter
+  class ResumeExtractor
+    def initialize(ollama_client)
+      @client = ollama_client
+    end
+  
+    def extract(text, schema)
+      prompt = PromptBuilder.build_extraction_prompt(text, schema)
+      @client.generate(
+        prompt: prompt,
+        temperature: 0.1,
+        max_tokens: 500
+    )
+    end
+  end
+end
